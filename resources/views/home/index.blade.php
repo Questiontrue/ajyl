5eda3962f80d298e81322f34bd561e40011fbcd5
Questiontrue <wonderful_qing@qq.com>
2018-07-27 22:12:15 +0800

    first commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,589 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>安景医联</title>
+		<link rel="stylesheet" type="text/css" href="css/currency.css" />
+		<style type="text/css">
+.banner {width: 100%;height:400px;overflow:hidden;background-image: url(images/banner_3.jpg);}
+
+.w-all {
+    width: 100%;
+    min-width: 1100px;
+}
+.w-main {
+    width: 1100px;
+    margin: 0 auto;
+}
+.conentl {
+    padding-top: 23px;
+}
+.fl {
+    float: left;
+}
+.conentfr {
+    padding-top: 23px;
+}
+
+.fr {
+    float: right;
+}
+.conentl .newstop {
+    width: 725px;
+    height: 43px;
+    line-height: 43px;
+    background-color: #1a86d3;
+    position: relative;
+}
+.conentl .newstop img {
+    position: absolute;
+    top: 10px;
+    left: 20px;
+}
+.conentl .newstop span {
+    position: absolute;
+    left: 56px;
+    color: #FFFFFF;
+    font-weight: bold;
+}
+.conentl .newstop a {
+    position: absolute;
+    right: 20px;
+    color: #FFFFFF;
+}
+.fs-12 {
+    font-size: 12px;
+}
+.newscont {
+    padding-top: 10px;
+}
+.newscont .newslist {
+    width: 340px;
+    padding-left: 15px;
+}
+.newscont .newsimg {
+    width: 366px;
+    height: 276px;
+    overflow: hidden;
+}
+.newscont .newsbott {
+    width: 366px;
+    height: 38px;
+    line-height: 38px;
+    background-color: #ebebeb;
+    font-size: 12px;
+    margin-top: -5px;
+}
+.newscont .newsbott .newspan1 {
+    color: #666666;
+    margin-left: 17px;
+    width: 265px;
+}
+.one_hidden {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+.newscont .newsbott .newspan2 {
+    color: #999999;
+}
+.newscont .newslist .newsfrtop {
+    height: 90px;
+    border-bottom: solid #e9e9e9 1px;
+    padding-top: 15px;
+}
+.newscont .newslist .newsfrtop h2 {
+    width: 265px;
+    color: #000000;
+}
+.fs-14 {
+    font-size: 14px;
+}
+.one_hidden {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+.newscont .newslist .newsfrtop span {
+    color: #999999;
+}
+.clear {
+    clear: both;
+}
+.newscont .newslist .newsfrtop p {
+    color: #666666;
+    line-height: 25px;
+    padding-top: 5px;
+}
+.fs-12 {
+    font-size: 12px;
+}
+.tw_hidden {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    overflow: hidden;
+}
+.newscont .newslist ul li {
+    padding-top: 19px;
+    height: 12px;
+}
+.newscont .newslist ul li h3 {
+    color: #000000;
+    width: 260px;
+    text-align: left;
+}
+.newscont .newslist ul li span {
+    color: #999999;
+}
+.one_hidden {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+.fl {
+    float: left;
+}
+
+
+.conentfr {
+    padding-top: 23px;
+}
+.rediantop {
+    width: 350px;
+    height: 43px;
+    line-height: 43px;
+    position: relative;
+}
+.zhuanti {
+    background-color: #2b9936;
+    width: 350px;
+}
+.rediantop img {
+    position: absolute;
+    top: 10px;
+    left: 20px;
+}
+.rediantop span {
+    position: absolute;
+    left: 60px;
+    color: #FFFFFF;
+}
+.fs-16 {
+    font-size: 16px;
+}
+
+
+
+
+.w-all {
+    width: 100%;
+    min-width: 1100px;
+}
+.w-main {
+    width: 1100px;
+    margin: 0 auto;
+}
+.massges {
+    margin-top: 20px;
+}
+.wtongy {
+    width: 25px;
+    height: 100px;
+}
+.xinxi {
+    background: url(images/xinxlogo.png) no-repeat;
+}
+.jiaoju {
+    background: url(images/jiaoju.png) no-repeat;
+}
+.zixu {
+    background: url(images/zixu.png) no-repeat;
+}
+.massgtop {
+    width: 350px;
+    height: 43px;
+    line-height: 46px;
+    position: relative;
+}
+.massgtop span {
+    position: absolute;
+    left: 50px;
+    color: #FFFFFF;
+    font-weight: bold;
+}
+.massgtop a {
+    position: absolute;
+    right: 20px;
+    color: #FFFFFF;
+}
+
+
+.massges ul {
+    width: 350px;
+}
+.massges ul li {
+    width: 325px;
+    height: 45px;
+    line-height: 45px;
+    border-bottom: solid #e0e0e0 1px;
+    margin-left: 15px;
+}
+.massges ul li h3 {
+    color: #000000;
+    width: 242px;
+    text-align: left;
+}
+.fs-12 {
+    font-size: 12px;
+}
+.one_hidden {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+.massges ul li {
+	line-height:45px;
+}
+
+.massges ul li span {
+    color: #999999;
+}
+
+
+		</style>
+	</head>
+	<body>
+		<!-- 头部 start-->
+		<!-- 页头start -->
+		<div class="header">
+			<div class="wl">
+				<a href="#" title="安景医联" class="a_home">安景医联首页</a>
+				<a href="javascript:(function(){window.open('http://v.t.sina.com.cn/share/share.php?title='+encodeURIComponent(document.title)+'&amp;url='+encodeURIComponent(location.href)+'&amp;source=bookmark','_blank');})()" title="新浪分享" class="a_sina"></a>
+				<div class="weixin">
+		        	<div class="icon" id="icon"></div>
+		        	<div class="img_b" id="img_b"><img src="images/ewm.jpg" alt=""></div>
+		      	</div>
+		      	<div class="links">
+		      		<a href="#" title="请登录">请登录</a>
+		      		<a href="#" title="免费注册">免费注册</a><span>丨</span>
+		      		<a href="#" title="我的订单">我的订单</a><span>丨</span>
+		      		<a href="#" title="联系客服">联系客服</a><span>丨</span>
+		      		<span class="fc_ff3253">咨询电话：+86-0000-96888</span><span>丨</span>
+		      	</div>
+			</div>
+		</div>
+		<!-- 页头end -->
+		
+		<!-- 头部logo+搜索 start-->
+		<div class="top_info">
+			<div class="wl">
+				<!-- logo -->
+				<div class="logo">
+					<a href="#" title="">
+						<img src="http://0001574.ks.panguweb.cn//Upload/atm/65BA01A1B44814289DDFEF30C8811E76.png" alt="">
+					</a>
+				</div>
+				<!-- 搜索 -->
+				<form action="">
+					<div class="search">
+          				<input type="text" name="sk" placeholder="输入您要查找的商品名称" class="in" value="">
+          			<input type="submit" class="btn_submit" value="">
+        		</div>
+				</form>
+				<!-- 购物车 -->
+				<div class="shop" id="end">
+					<a id="gwc" href="#" title="" target="_blank">购物车<span>0</span>件
+					</a>
+				</div>
+			</div>
+		</div>
+		<!-- 头部logo+搜索 end-->
+		<!-- 头部end -->
+		
+		
+		<!-- 导航条start -->
+		<div class="nav-bar">
+			<div class="nav-bg">
+				<div class="nav-top">
+					<div class="nav-content">
+						<ul class="nav-content-box">
+							<li class="special ">
+								<span><a href="javascript:void(0)">首页</a></span>
+								<ul>
+									<li><a href="#">血液送检</a></li>
+									<li><a href="#">基因送检</a></li>
+									<li><a href="#">我的客户</a></li>
+									<li><a href="#">医生助手</a></li>
+									<li><a href="#">机构资料</a></li>
+									<li><a href="#">在线充值</a></li>
+								</ul>
+							</li>
+							<li class="special ">
+								<span><a href="javascript:void(0)">产品与服务</a></span>
+								<ul>
+									<li><a href="#">血液送检</a></li>
+									<li><a href="#">基因送检</a></li>
+									<li><a href="#">我的客户</a></li>
+									<li><a href="#">医生助手</a></li>
+									<li><a href="#">机构资料</a></li>
+									<li><a href="#">在线充值</a></li>
+								</ul>
+							</li>
+							<li class="special ">
+								<span><a href="javascript:void(0)">诊所合作</a></span>
+								<ul>
+									<li><a href="#">血液送检</a></li>
+									<li><a href="#">基因送检</a></li>
+									<li><a href="#">我的客户</a></li>
+									<li><a href="#">医生助手</a></li>
+									<li><a href="#">机构资料</a></li>
+									<li><a href="#">在线充值</a></li>
+								</ul>
+							</li>
+							<li class="special ">
+								<span><a href="javascript:void(0)">大健康</a></span>
+								<ul>
+									<li><a href="#">病理百科</a></li>
+									<li><a href="#">用药指导</a></li>
+									<li><a href="#">经典问答</a></li>
+									<li><a href="#">前沿咨询</a></li>
+								</ul>
+							</li>
+							<li class="index on">
+								<span><a href="#">微诊所</a></span>
+							</li>
+							<li class="index on">
+								<span><a href="#">加入我们</a></span>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- 导航条end -->
+		
+		
+		<!-- banner start -->
+		<div class="banner">
+			
+		</div>
+		<!-- banner end -->
+		
+		
+		<div class="w-all">
+			<div class="w-main">
+				<div class="fl conentl">
+					<div class="newstop">
+						<img src="http://www.17sucai.com/preview/356806/2018-01-23/shengyiyaopc/img/newslogo.png" alt="">
+						<span class="fs-16">病理百科</span>
+						<a href="" class="fs-12">MORE &gt;</a>
+					</div>
+					<div class="newscont">
+						<div class="fl">
+							<div class="newsimg">
+								<a href=""><img src="http://www.17sucai.com/preview/356806/2018-01-23/shengyiyaopc/img/contnew.png" alt=""></a>
+							</div>
+							<div class="newsbott">
+								<a href="">
+									<p class="one_hidden newspan1 fl">集团工会深入公司进</p>
+									<p class="newspan2 fl">2017-01-01</p>
+								</a>
+							</div>
+						</div>
+						<div class="fl newslist">
+							<div class="newsfrtop">
+								<a href="">
+									<h2 class="fs-14 fl one_hidden">集团工会深入公司进行调研指导工作</h2>
+									<span class="fs-12 fr">2016-12-12</span>
+									<div class="clear"></div>
+									<p class="fs-12 tw_hidden">为了贯彻集团工作要求，深化主题劳动竞赛促进企业发展，推动“点赞医药好职工”活动有序开展……</p>
+								</a>
+							</div>
+							<ul>
+								<li>
+									<a href="">
+										<h3 class="fs-12 fl one_hidden">· 国药器械50传承活动圆满结束</h3>
+										<span class="fs-12 fr">2016-12-12</span>
+									</a>
+								</li>
+								<li>
+									<a href="">
+										<h3 class="fs-12 fl one_hidden">· 国药器械50传承活动圆满结束</h3>
+										<span class="fs-12 fr">2016-12-12</span>
+									</a>
+								</li>
+								<li>
+									<a href="">
+										<h3 class="fs-12 fl one_hidden">· 国药器械50传承活动圆满结束</h3>
+										<span class="fs-12 fr">2016-12-12</span>
+									</a>
+								</li>
+								<li>
+									<a href="">
+										<h3 class="fs-12 fl one_hidden">· 国药器械50传承活动圆满结束</h3>
+										<span class="fs-12 fr">2016-12-12</span>
+									</a>
+								</li>
+								<li>
+									<a href="">
+										<h3 class="fs-12 fl one_hidden">· 国药器械50传承活动圆满结束</h3>
+										<span class="fs-12 fr">2016-12-12</span>
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<h3 class="fs-12 fl one_hidden">· 国药器械50传承活动圆满结束</h3>
+										<span class="fs-12 fr">2016-12-12</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="fr conentfr">
+					<div class="rediantop zhuanti">
+						<img src="http://www.17sucai.com/preview/356806/2018-01-23/shengyiyaopc/img/redainlogo.png" alt="">
+						<span class="fs-16">热点专题</span>
+					</div>
+					<a href=""><img src="http://www.17sucai.com/preview/356806/2018-01-23/shengyiyaopc/img/redian01.png" alt=""></a><br>
+					<a href=""><img src="http://www.17sucai.com/preview/356806/2018-01-23/shengyiyaopc/img/redian02.png" alt=""></a><br>
+					<a href=""><img src="http://www.17sucai.com/preview/356806/2018-01-23/shengyiyaopc/img/redian03.png" alt=""></a><br>
+					<a href=""><img src="http://www.17sucai.com/preview/356806/2018-01-23/shengyiyaopc/img/redian04.png" alt=""></a>
+				</div>
+			</div>
+		</div>
+		<div class="clear"></div>
+		
+		
+		<div class="w-all">
+			<div class="w-main">
+				<div class="fl massges">
+						<div class="massgtop xinxi">
+							<span class="fs-16">用药指导</span>
+							<a href="" class="fs-12">MORE &gt;</a>
+						</div>
+						<ul>
+							<li>
+								<a href="">
+										<h3 class="fs-12 fl one_hidden">国药器械50传承活动圆满结束</h3>
+										<span class="fs-12 fr">2016-12-12</span>
+									</a>
+							</li>
+							<li>
+								<a href="">
+										<h3 class="fs-12 fl one_hidden">国药器械50传承活动圆满结束</h3>
+										<span class="fs-12 fr">2016-12-12</span>
+									</a>
+							</li>
+							<li>
+								<a href="">
+										<h3 class="fs-12 fl one_hidden">国药器械50传承活动圆满结束</h3>
+										<span class="fs-12 fr">2016-12-12</span>
+									</a>
+							</li>
+							<li>
+								<a href="">
+										<h3 class="fs-12 fl one_hidden">国药器械50传承活动圆满结束</h3>
+										<span class="fs-12 fr">2016-12-12</span>
+									</a>
+							</li>
+						</ul>
+					</div>
+				<div class="fl wtongy"></div>
+				<div class="fl massges ">
+					<div class="massgtop jiaoju">
+						<span class="fs-16">经典问答</span>
+						<a href="" class="fs-12">MORE &gt;</a>
+					</div>
+					<ul>
+						<li>
+							<a href="">
+									<h3 class="fs-12 fl one_hidden">国药器械50传承活动圆满结束</h3>
+									<span class="fs-12 fr">2016-12-12</span>
+								</a>
+						</li>
+						<li>
+							<a href="">
+									<h3 class="fs-12 fl one_hidden">国药器械50传承活动圆满结束</h3>
+									<span class="fs-12 fr">2016-12-12</span>
+								</a>
+						</li>
+						<li>
+							<a href="">
+									<h3 class="fs-12 fl one_hidden">国药器械50传承活动圆满结束</h3>
+									<span class="fs-12 fr">2016-12-12</span>
+								</a>
+						</li>
+						<li>
+							<a href="">
+									<h3 class="fs-12 fl one_hidden">国药器械50传承活动圆满结束</h3>
+									<span class="fs-12 fr">2016-12-12</span>
+								</a>
+						</li>
+					</ul>
+				</div>
+				<div class="fl wtongy"></div>
+				<div class="fl massges ">
+					<div class="massgtop zixu">
+						<span class="fs-16">前沿咨询</span>
+						<a href="" class="fs-12">MORE &gt;</a>
+					</div>
+					<ul>
+						<li>
+							<a href="">
+									<h3 class="fs-12 fl one_hidden">国药器械50传承活动圆满结束</h3>
+									<span class="fs-12 fr">2016-12-12</span>
+								</a>
+						</li>
+						<li>
+							<a href="">
+									<h3 class="fs-12 fl one_hidden">国药器械50传承活动圆满结束</h3>
+									<span class="fs-12 fr">2016-12-12</span>
+								</a>
+						</li>
+						<li>
+							<a href="">
+									<h3 class="fs-12 fl one_hidden">国药器械50传承活动圆满结束</h3>
+									<span class="fs-12 fr">2016-12-12</span>
+								</a>
+						</li>
+						<li>
+							<a href="">
+									<h3 class="fs-12 fl one_hidden">国药器械50传承活动圆满结束</h3>
+									<span class="fs-12 fr">2016-12-12</span>
+								</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="clear"></div>
+		
+		
+		<!-- footer start-->
+		<div class="foonavf">
+			<div class="wl">
+		        	
+		            <div class="nnf">
+		            	<a class="foonav" href="/index.php?m=cn&amp;c=Index&amp;a=index" title="网站首页">网站首页</a> <span>  |  </span><a class="foonav" href="/index.php?m=cn&amp;c=About&amp;a=index" title="产品与服务">产品与服务</a> <span>  |  </span><a class="foonav" href="/index.php?m=cn&amp;c=Adv&amp;a=index" title="诊所合作">诊所合作</a> <span>  |  </span><a class="foonav" href="/index.php?m=cn&amp;c=Product&amp;a=index" title="大健康">大健康</a> <span>  |  </span><a class="foonav" href="/index.php?m=cn&amp;c=News&amp;a=index" title="微诊所">微诊所</a> <span>  |  </span><a class="foonav" href="/index.php?m=cn&amp;c=Case&amp;a=index" title="加入我们">加入我们</a> <span>  |  </span></div>
+		            <div class="rb"><a href="" title="网站地图" class="map_link">网站地图（xml / html）</a></div>
+		     </div>
+		</div>
+		<div class="footer">
+			<div class="wl">
+		    		<div class="manager_info"><span>电话：+86-0000-96888</span><span>传真：+86-0000-96888</span><span>地址：这里是您的公司地址</span><br><span>版权所有：这里是您的网站名称</span><span>技术支持：<a href="#" title="WEB开发平台" target="_blank">WEB开发平台</a></span> <span><a href="http://www.miitbeian.gov.cn" title="备案信息">ICP备********号</a></span></div>
+		            <div class="share_b">
+		                
+		               <div class="bdsharebuttonbox bdshare-button-style0-32" data-bd-bind="1529332784797"><a href="#" class="bds_more" data-cmd="more"></a><a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a><a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a><a href="#" class="bds_tqq" data-cmd="tqq" title="分享到腾讯微博"></a><a href="#" class="bds_renren" data-cmd="renren" title="分享到人人网"></a><a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a></div>
+		<script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"32"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
+		            </div>
+		   </div>
+		</div>
+		<!-- footer end-->
+		
+		
+		
+		<script src="js/onload.js" type="text/javascript" charset="utf-8"></script>
+	</body>
+</html>
